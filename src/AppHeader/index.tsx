320fc012b972d896c2a69c0ec9c5544ae1d28f98
bowei <bowei.han100@gmail.com>
2020-04-18 13:11:30 -0400

    nav responsiveness

--- src/AppHeader/index.tsx
+++ src/AppHeader/index.tsx
@@ -1,24 +1,55 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { PlusOutlined } from '@ant-design/icons';
+import { PlusCircleFilled, ProfileFilled } from '@ant-design/icons';
 import { Menu } from 'antd';
+import Media from 'react-media';
 import styles from './styles.css';
 import 'antd/es/menu/style';
 
 const AppHeader = () => (
   <>
-    <h1 className={styles.logo}>CIF</h1>
-    <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-      <Menu.Item key="1">
-        <Link to="/create">
-          <PlusOutlined />
-          Submit
-        </Link>
-      </Menu.Item>
-      <Menu.Item key="2">
-        <Link to="/public">Featured</Link>
-      </Menu.Item>
-    </Menu>
+    <h1 className={styles.logo}>CodingBlindspots</h1>
+    <Media
+      queries={{
+        small: '(max-width: 600px)',
+        large: '(min-width: 601px)',
+      }}
+    >
+      {(matches) => (
+        <>
+          {matches.small && (
+            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+              <Menu.Item key="1">
+                <Link to="/create" className={styles.smallIconLink}>
+                  <PlusCircleFilled />
+                </Link>
+              </Menu.Item>
+              <Menu.Item key="2">
+                <Link to="/public" className={styles.smallIconLink}>
+                  <ProfileFilled />
+                </Link>
+              </Menu.Item>
+            </Menu>
+          )}
+          {matches.large && (
+            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+              <Menu.Item key="1">
+                <Link to="/create">
+                  <PlusCircleFilled />
+                  Submit
+                </Link>
+              </Menu.Item>
+              <Menu.Item key="2">
+                <Link to="/public">
+                  <ProfileFilled />
+                  Featured
+                </Link>
+              </Menu.Item>
+            </Menu>
+          )}
+        </>
+      )}
+    </Media>
   </>
 );
 
