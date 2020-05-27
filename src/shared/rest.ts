b5bea05b26faa5e3b82e559acacd6805992d981a
mrraghur <mrraghur@x.com>
2020-05-27 10:30:26 -0700

    Implement a proxy.

--- src/shared/rest.ts
+++ src/shared/rest.ts
@@ -1,9 +1,10 @@
 import axios from 'axios';
 import config from '../config';
 
+const proxyconfig = {proxy: { host: '127.0.0.1', port: 8000}};
 const get = (path: string) => {
   return axios
-    .get(`${config.baseUrl}${path}`)
+    .get(`${config.baseUrl}${path}`, proxyconfig)
     .then((response) => {
       return response.data;
     })
@@ -11,6 +12,8 @@ const get = (path: string) => {
       console.log(error);
     });
 };
+
+
 
 const post = (path: string, body: object) => {
   return axios
@@ -23,4 +26,4 @@ const post = (path: string, body: object) => {
     });
 };
 
-export default { get, post };
+export default { get, post};
