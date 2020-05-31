e5f33193bb106f0e9c28527ae6571fa9ef62606a
mrraghur <mrraghur@x.com>
2020-05-30 23:07:55 -0700

    Add backend/frontend urls

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,7 @@
-const baseUrl = 'http://localhost:8080';
+//This should be the backend URL so you can proxy the request to it
+const backendurl = 'http://localhost:8000' 
+const frontendurl = 'http://localhost:8080' 
+
+const baseUrl = frontendurl;
 
 export default { baseUrl };
